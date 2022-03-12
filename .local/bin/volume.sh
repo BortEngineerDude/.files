31173a82e06aa13c622ad5b201d3eac8f34df4bf
BortEngineer <dmitriy2109@gmai.com>
2022-03-12 11:12:19 +0300

    -Fixed blocking volume control

--- .local/bin/volume.sh
+++ .local/bin/volume.sh
@@ -7,28 +7,23 @@ VOLUME_33=奔
 VOLUME_66=墳
 VOLUME_MUTE=婢
 
-if [ $button ]; then 
-    bash -c 'nohup pavucontrol &';
+COLOR="NULL"
+ICON="NULL"
+VOLUME=`pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}'`;
+VOLUME=${VOLUME%?}
+MUTE=`LC_ALL=C pactl get-sink-mute @DEFAULT_SINK@` 
+
+if [ "$MUTE" = "Mute: yes" ]; then
+    ICON=VOLUME_MUTE
+    COLOR="#FF8000"
 else
-    COLOR="NULL"
-    ICON="NULL"
-    VOLUME=`pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}'`;
-    VOLUME=${VOLUME%?}
-    MUTE=`LC_ALL=C pactl get-sink-mute @DEFAULT_SINK@` 
-
-    if [ "$MUTE" = "Mute: yes" ]; then
-        ICON=VOLUME_MUTE
-        COLOR="#FF8000"
-    else
-        COLOR="#FFFFFF"
-        for PERCENT in `seq 0 33 66`; do
-            if [ $PERCENT -le $VOLUME ]; then
-                ICON=VOLUME_$PERCENT
-            fi
-        done
-    fi
-
-    TEXT="${!ICON} $VOLUME%"
-    printf '{"full_text": "%s", "color": "%s"}\n' "$TEXT" "$COLOR"
+    COLOR="#FFFFFF"
+    for PERCENT in `seq 0 33 66`; do
+        if [ $PERCENT -le $VOLUME ]; then
+            ICON=VOLUME_$PERCENT
+        fi
+    done
 fi
 
+TEXT="${!ICON} $VOLUME%"
+printf '{"full_text": "%s", "color": "%s"}\n' "$TEXT" "$COLOR"
