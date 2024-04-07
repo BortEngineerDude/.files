5d1f6db144a713e11f324fcf17b91a7cea32c4b7
BortEngineer <who@car.es>
2024-04-07 12:55:37 +0200

    -Improve battery monitoring

--- .local/bin/batterypercent.sh
+++ .local/bin/batterypercent.sh
@@ -1,30 +1,9 @@
-#!/bin/sh
+#!/bin/env bash
 
-#Battery script for i3blocks with nerdfont
+# Battery script for i3blocks with nerdfont
 
-DISCHARGE_0=󱃍
-DISCHARGE_10=󰁺
-DISCHARGE_20=󰁻
-DISCHARGE_30=󰁼
-DISCHARGE_40=󰁽
-DISCHARGE_50=󰁾
-DISCHARGE_60=󰁿
-DISCHARGE_70=󰂀
-DISCHARGE_80=󰂁
-DISCHARGE_90=󰂂
-DISCHARGE_100=󰁹
-
-CHARGE_0=󰢟
-CHARGE_10=󰢜
-CHARGE_20=󰂆
-CHARGE_30=󰂇
-CHARGE_40=󰂈
-CHARGE_50=󰢝
-CHARGE_60=󰂉
-CHARGE_70=󰢞
-CHARGE_80=󰂊
-CHARGE_90=󰂋
-CHARGE_100=󰂅
+ICONS_DISCHARGING=(󱃍 󰁺 󰁻 󰁼 󰁽 󰁾 󰁿 󰂀 󰂁 󰂂 󰁹)
+ICONS_CHARGING=(󰢟 󰢜 󰂆 󰂇 󰂈 󰢝 󰂉 󰢞 󰂊 󰂋 󰂅)
 
 ACPI=`acpi -ab`
 CHARGING=`echo "$ACPI" | grep -o on-line`
@@ -32,27 +11,47 @@ CHARGE=`echo "$ACPI" | grep -E "[0-9]{1,3}%" -o`
 CHARGE=${CHARGE%?}
 
 for PERCENT in `seq 100 -10 0`; do
-    if [ $CHARGE -ge $PERCENT ]; then
-        ICON="CHARGE_$PERCENT"
-        if [ -z $CHARGING ]; then
-            ICON="DIS$ICON"
-        fi
-        
-        TEXT="${!ICON} $CHARGE%"
-        
-        if [ $CHARGE -le 20 ]; then
-            COLOR="#FF8000"                  
-        else
-            COLOR="#FFFFFF"
-        fi
+  if [ $CHARGE -ge $PERCENT ]; then
+    ICON_IDX=$(($PERCENT / 10))
 
-        if [ $CHARGE -le 5 ]; then
-            URGENT="true"                  
-        else
-            URGENT="false"                  
-        fi
-
-        printf '{"full_text": "%s", "color": "%s", "urgent": "%s"}\n' "$TEXT" "$COLOR" "$URGENT"
-        exit
+    ICON=ICONS_CHARGING[$ICON_IDX]  
+    if [ -z $CHARGING ]; then
+      ICON=ICONS_DISCHARGING[$ICON_IDX]  
     fi
+    
+    TEXT="${!ICON} $CHARGE%"
+    
+    if [ $CHARGE -le 20 ]; then
+        COLOR="#FF8000"                  
+    else
+        COLOR="#FFFFFF"
+    fi
+
+    if [ $CHARGE -le 10 ]; then
+        URGENT="true"                  
+    else
+        URGENT="false"                  
+    fi
+
+    printf '{"full_text": "%s", "color": "%s", "urgent": "%s"}\n' "$TEXT" "$COLOR" "$URGENT"
+    break
+  fi
 done
+
+ALARM_LOCK="/tmp/sb-bat.lock"
+ALARM="/usr/share/sounds/freedesktop/stereo/suspend-error.oga"
+
+if [ -z $CHARGING  ] && [ "$CHARGE" -le 10 ]; then
+	if  [ ! -f "$ALARM_LOCK" ]; then
+		mpv --volume=60 --no-video "$ALARM" >/dev/null 2>&1 &
+		touch "$ALARM_LOCK"
+		notify-send "Battery low!" -h string:x-dunst-stack-tag:battery -i battery-level-10-symbolic
+	fi
+
+  if [ "$CHARGE" -le 5 ]; then
+    systemctl suspend
+  fi
+
+else
+	rm -f "$ALARM_LOCK"
+fi
